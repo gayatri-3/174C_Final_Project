afaa9ccb8bb766bfa148c6139471c24d47ccca9f
cronkubine <eatoneer@g.ucla.edu>
2024-03-15 22:35:00 -0700

    added gangnam style human

--- bumper_cars.js
+++ bumper_cars.js
@@ -1,5 +1,6 @@
 import {tiny, defs} from './examples/common.js';
 import { Shape_From_File } from './examples/obj-file-demo.js';
+import { Articulated_Human } from '../cs-174c-assignment-2/human.js';
 
 // Pull these names into this module's scope for convenience:
 const { vec3, vec4, color, Mat4, Shape, Material, Shader, Texture, Component } = tiny;
@@ -101,7 +102,8 @@ const Bumper_cars_base = defs.Bumper_cars_base =
           'ball' : new defs.Subdivision_Sphere( 4 ),
           'axis' : new defs.Axis_Arrows(),
           'sky': new defs.Subdivision_Sphere(4),
-          'fence' : new Shape_From_File("./assets/fence/fence.obj")
+          'fence' : new Shape_From_File("./assets/fence/fence.obj"),
+          'human': new Articulated_Human(),
         };
 
         this.curve_fn = null;
@@ -123,6 +125,7 @@ const Bumper_cars_base = defs.Bumper_cars_base =
         this.materials.sky = {shader: tex_phong, ambient: 1, texture: new Texture("assets/sky.png")}
         //this.materials.sky = {shader: tex_phong, ambient: 1, texture: new Texture("assets/sky_cartoon.png")}
         this.materials.ground = {shader: tex_phong, ambient: 1, texture: new Texture("assets/grass_1.jpg")}
+        this.materials.flesh   = { shader: phong, ambient: .2, diffusivity: 1, specularity:  0, color: color( .9,.5,.9,1 ) }
         //this.materials.ground = {shader: tex_phong, ambient: 1, texture: new Texture("assets/grass_cartoon.png")}
         this.ball_location = vec3(1, 1, 1);
         this.ball_radius = 0.25;
@@ -217,6 +220,27 @@ const Bumper_cars_base = defs.Bumper_cars_base =
         this.fireworks_animation = false;
         //this.fireworks = new FireworksDisplay(10, 10, 10, 2);
 
+        // animatronic
+        this.spline = new Spline();
+        this.spline.add_point(-3.25, 7.0, -0.85, 1, 0.0, 0.0);
+        this.spline.add_point(-3.25, 5.0, -0.85, -1, 0.0, 0.0);
+        this.spline.add_point(-3.25, 7.0, -0.85, -1, 0.0, 0.0);
+
+        this.spline2 = new Spline();
+        this.spline2.add_point(0, 8.0, -0.85, -1, 0.0, 0.0);
+        this.spline2.add_point(0, 6.0, -0.85, 1, 0.0, 0.0);
+        this.spline2.add_point(0, 8.0, -0.85, 2, 0.0, 0.0);
+        
+        const curve_fn = (t) => this.spline.get_position(t);
+        const curve_fn2 = (t) => this.spline2.get_position(t);
+        this.sample_cnt = 1000;
+        this.curve = new Curve_Shape(curve_fn, this.sample_cnt);
+        this.curve2 = new Curve_Shape(curve_fn2, this.sample_cnt2);
+
+        this.human = new Articulated_Human;
+        this.right_target_pos = vec3(-2.25, 6.0, -0.85);
+        this.left_target_pos = vec3(1.25, 6.0, -0.85);
+        this.left_target_pos = vec3(1.25, 2.0, -0.85);
       }
 
       render_animation( caller )
@@ -244,7 +268,7 @@ const Bumper_cars_base = defs.Bumper_cars_base =
 
         // *** Lights: *** Values of vector or point lights.  They'll be consulted by
         // the shader when coloring shapes.  See Light's class definition for inputs.
-        const t = this.t = this.uniforms.animation_time/1000;
+        let t = this.t = this.uniforms.animation_time/1000;
         //const angle = Math.sin( t );
         const angle = Math.sin( 0 );
 
@@ -310,9 +334,9 @@ export class Bumper_cars extends Bumper_cars_base
         // translation(), scale(), and rotation() to generate matrices, and the
         // function times(), which generates products of matrices.
 
-    const blue = color( 0,0,1,1 ), yellow = color( 1,0.7,0,1 ), red = color( 1,0,1.0,1.0, 1);
+    const blue = color( 0,0,1,1 ), yellow = color( 1,0.7,0,1 ), red = color( 1,0,1.0,1.0, 1), flesh = color(1, 0.79, 0.64, 1);
 
-    const t = this.t = this.uniforms.animation_time/1000;
+    let t = this.t = this.uniforms.animation_time/1000;
 
     // !!! Draw ball (for reference)
     let ball_transform = Mat4.translation(this.ball_location[0], this.ball_location[1], this.ball_location[2])
@@ -423,9 +447,72 @@ export class Bumper_cars extends Bumper_cars_base
       lastTimestamp = currentTime;
       this.fireworks.update(dt);
       this.fireworks.draw(caller, this.uniforms, this.shapes, this.materials);
-
     }
 
+    // animatronic
+    let lu_leg_transform = Mat4.scale(0.2, 0.8, .3);
+    lu_leg_transform.pre_multiply(Mat4.translation(-2.1, 2.5, 2));
+    this.shapes.ball.draw(caller, this.uniforms, lu_leg_transform, { ...this.materials.flesh, color : flesh});
+    let ll_leg_transform = Mat4.scale(0.2, 1, .1);
+    ll_leg_transform.pre_multiply(Mat4.translation(-2.1, 0.9, 2));
+    this.shapes.ball.draw(caller, this.uniforms, ll_leg_transform, { ...this.materials.flesh, color : flesh});
+    let ru_leg_transform = Mat4.scale(0.2, 0.8, .3);
+    ru_leg_transform.pre_multiply(Mat4.translation(-0.9, 2.5, 2));
+    this.shapes.ball.draw(caller, this.uniforms, ru_leg_transform, { ...this.materials.flesh, color : flesh});
+    let rl_leg_transform = Mat4.scale(0.2, 1, .1);
+    rl_leg_transform.pre_multiply(Mat4.translation(-0.9, 0.9, 2));
+    this.shapes.ball.draw(caller, this.uniforms, rl_leg_transform, { ...this.materials.flesh, color : flesh});
+    let l_foot_transform = Mat4.scale(0.5, 0.5, 0.5);
+    l_foot_transform.pre_multiply(Mat4.translation(-2.1, 0.3, 1.65));
+    this.shapes.ball.draw(caller, this.uniforms, l_foot_transform, { ...this.materials.flesh, color : flesh});
+    let r_foot_transform = Mat4.scale(0.5, 0.5, .5);
+    r_foot_transform.pre_multiply(Mat4.translation(-0.9, 0.3, 1.65));
+    this.shapes.ball.draw(caller, this.uniforms, r_foot_transform, { ...this.materials.flesh, color : flesh});
+    this.human.draw(caller, this.uniforms, { ...this.materials.flesh, color : flesh});
+
+    const dt_adjusted = Math.min(1 / 30, this.uniforms.animation_delta_time / 1000);
+    const t_next = t + dt_adjusted;
+    this.right_target_pos = Array.from(this.spline.get_position(this.t - Math.floor(this.t)));
+    this.left_target_pos = Array.from(this.spline2.get_position(this.t - Math.floor(this.t)));
+    for (; t <= t_next; t += 1/20 ){
+      const k = 0.029;
+      let right_curr_pos = Array.from(this.human.get_right_end_effector_position());
+      let left_curr_pos = Array.from(this.human.get_left_end_effector_position())
+
+      let right_E = math.subtract(this.right_target_pos, right_curr_pos);
+      let left_E = math.subtract(this.left_target_pos, left_curr_pos);
+
+
+      let right_error_dist = Math.sqrt(right_E[0] ** 2 + right_E[1] ** 2 + right_E[2] ** 2);
+      let left_error_dist = Math.sqrt(left_E[0] ** 2 + left_E[1] ** 2 + left_E[2] ** 2);
+
+      if (right_error_dist >= k){
+        let dx = math.multiply(10, math.multiply(k, right_E));
+        let right_J = this.human.calculate_right_Jacobian();
+        let Jinv = this.human.calculate_inverse_Jacobian(right_J);
+        let dtheta = math.multiply(math.transpose(dx), Jinv);
+        this.human.right_theta = math.add(this.human.right_theta, dtheta);
+        let sum = new Array(3);
+        for (let i = 0; i < 3; i++){
+          sum[i] = this.human.right_theta[i] + dtheta[i];
+        }
+      }
+      
+      const k2 = 0.01;
+      if (left_error_dist >= k2){
+        let dx = math.multiply(7, math.multiply(k2, left_E));
+        let left_J = this.human.calculate_left_Jacobian();
+        let Jinv = this.human.calculate_inverse_Jacobian(left_J);
+        let dtheta = math.multiply(math.transpose(dx), Jinv);
+        this.human.left_theta = math.add(this.human.left_theta, dtheta);
+        let sum = new Array(3);
+        for (let i = 0; i < 3; i++){
+          sum[i] = this.human.left_theta[i] + dtheta[i];
+        }
+      }
+
+    }
+    this.initial_set = true;
   }
 
   render_controls()
---
+++ human.js
@@ -0,0 +1,335 @@
+import {tiny, defs} from './examples/common.js';
+
+// import './linear-algebra-js/node/linear-algebra.js';
+// Pull these names into this module's scope for convenience:
+const { vec3, vec4, color, Mat4, Shape, Material, Shader, Texture, Component } = tiny;
+
+const shapes = {
+    'sphere': new defs.Subdivision_Sphere( 5 ),
+};
+
+export
+const Articulated_Human = 
+class Articulated_Human {
+    constructor() {
+        const sphere_shape = shapes.sphere;
+
+        // torso node
+        const torso_transform = Mat4.scale(1, 2.5, 0.5);
+        this.torso_node = new Node("torso", sphere_shape, torso_transform);
+        // root->torso
+        const root_location = Mat4.translation(-1.5, 5, 2);
+        this.root = new Arc("root", null, this.torso_node, root_location);
+
+        // head node
+        let head_transform = Mat4.scale(.6, .6, .6);
+        head_transform.pre_multiply(Mat4.translation(0, .6, 0));
+        this.head_node = new Node("head", sphere_shape, head_transform);
+        // torso->neck->head
+        const neck_location = Mat4.translation(0, 2.5, 0);
+        this.neck = new Arc("neck", this.torso_node, this.head_node, neck_location);
+        this.torso_node.children_arcs.push(this.neck);
+
+        // right upper arm node
+        let ru_arm_transform = Mat4.scale(1.2, .2, .2);
+        ru_arm_transform.pre_multiply(Mat4.translation(1.2, 0, 0));
+        this.ru_arm_node = new Node("ru_arm", sphere_shape, ru_arm_transform);
+        // torso->r_shoulder->ru_arm
+        const r_shoulder_location = Mat4.translation(0.6, 2, 0);
+        this.r_shoulder = new Arc("r_shoulder", this.torso_node, this.ru_arm_node, r_shoulder_location);
+        this.torso_node.children_arcs.push(this.r_shoulder)
+        this.r_shoulder.set_dof(true, true, true);
+
+        // right lower arm node
+        let rl_arm_transform = Mat4.scale(1, .2, .2);
+        rl_arm_transform.pre_multiply(Mat4.translation(1, 0, 0));
+        this.rl_arm_node = new Node("rl_arm", sphere_shape, rl_arm_transform);
+        // ru_arm->r_elbow->rl_arm
+        const r_elbow_location = Mat4.translation(2.4, 0, 0);
+        this.r_elbow = new Arc("r_elbow", this.ru_arm_node, this.rl_arm_node, r_elbow_location);
+        this.ru_arm_node.children_arcs.push(this.r_elbow)
+        this.r_elbow.set_dof(true, true, false);
+
+        // right hand node
+        let r_hand_transform = Mat4.scale(.4, .3, .2);
+        r_hand_transform.pre_multiply(Mat4.translation(0.4, 0, 0));
+        this.r_hand_node = new Node("r_hand", sphere_shape, r_hand_transform);
+        // rl_arm->r_wrist->r_hand
+        const r_wrist_location = Mat4.translation(2, 0, 0);
+        this.r_wrist = new Arc("r_wrist", this.rl_arm_node, this.r_hand_node, r_wrist_location);
+        this.rl_arm_node.children_arcs.push(this.r_wrist);
+        this.r_wrist.set_dof(false, true, true);
+        const r_hand_end_local_pos = vec4(0.8, 0, 0, 1);
+        this.right_end_effector = new End_Effector("right_hand", this.r_wrist, r_hand_end_local_pos);
+        this.r_wrist.end_effector = this.right_end_effector;
+        this.dof = 7;
+        this.right_theta = [0, 0, 0, 0, 0, 0, 0];
+        
+        // left upper arm node
+        let lu_arm_transform = Mat4.scale(1.2, .2, .2);
+        lu_arm_transform.pre_multiply(Mat4.translation(-1.2, 0, 0));
+        this.lu_arm_node = new Node("lu_arm", sphere_shape, lu_arm_transform);
+        // torso->r_shoulder->ru_arm
+        const l_shoulder_location = Mat4.translation(-0.6, 2, 0);
+        this.l_shoulder = new Arc("l_shoulder", this.torso_node, this.lu_arm_node, l_shoulder_location);
+        this.torso_node.children_arcs.push(this.l_shoulder)
+        this.l_shoulder.set_dof(true, true, true);
+
+         // left lower arm node
+        let ll_arm_transform = Mat4.scale(1, .2, .2);
+        ll_arm_transform.pre_multiply(Mat4.translation(-1, 0, 0));
+        this.ll_arm_node = new Node("ll_arm", sphere_shape, ll_arm_transform);
+        // lu_arm->l_elbow->ll_arm
+        const l_elbow_location = Mat4.translation(-2.4, 0, 0);
+        this.l_elbow = new Arc("l_elbow", this.lu_arm_node, this.ll_arm_node, l_elbow_location);
+        this.lu_arm_node.children_arcs.push(this.l_elbow)
+        this.l_elbow.set_dof(true, true, false);
+
+        // left hand node
+        let l_hand_transform = Mat4.scale(.4, .3, .2);
+        l_hand_transform.pre_multiply(Mat4.translation(-0.4, 0, 0));
+        this.l_hand_node = new Node("l_hand", sphere_shape, l_hand_transform);
+        // rl_arm->r_wrist->r_hand
+        const l_wrist_location = Mat4.translation(-2, 0, 0);
+        this.l_wrist = new Arc("l_wrist", this.ll_arm_node, this.l_hand_node, l_wrist_location);
+        this.ll_arm_node.children_arcs.push(this.l_wrist);
+        this.l_wrist.set_dof(false, true, true);
+        const l_hand_end_local_pos = vec4(0.8, 0, 0, 1);
+        this.left_end_effector = new End_Effector("left_hand", this.l_wrist, l_hand_end_local_pos);
+        this.l_wrist.end_effector = this.left_end_effector;
+        this.dof = 7;
+        this.left_theta = [0, 0, 0, 0, 0, 0, 0];        
+    }
+
+    // mapping from global theta to each joint theta
+    apply_theta() {
+        this.r_shoulder.update_articulation(this.right_theta.slice(0, 3));
+        this.r_elbow.update_articulation(this.right_theta.slice(3, 5));
+        this.r_wrist.update_articulation(this.right_theta.slice(5, 7));
+        
+        this.l_shoulder.update_articulation(this.left_theta.slice(0, 3));
+        this.l_elbow.update_articulation(this.left_theta.slice(3, 5));
+        this.l_wrist.update_articulation(this.left_theta.slice(5, 7));
+    }
+
+    calculate_right_Jacobian() {
+        // use newton's method in order to find the jacobian
+        const delta = 1e-4;
+        let J = new Array(this.dof);
+        for (let i = 0; i < this.dof; i++) {
+            J[i] = new Array(3).fill(0);
+        }
+        const right_orig_pos = this.get_right_end_effector_position();
+        // TODO: Implement your Jacobian here
+        for (let i = 0; i < this.dof; i++){
+            this.right_theta[i] += delta;
+            this.apply_theta();
+            const right_new_pos = this.get_right_end_effector_position();
+            for (let j = 0; j < 3; j++)
+            J[i][j] = (right_new_pos[j] - right_orig_pos[j]) / delta;
+        this.apply_theta();
+        this.right_theta[i] -= delta;
+        }
+        return J; // 3x7 in my case.
+    }
+
+    calculate_left_Jacobian() {
+        // use newton's method in order to find the jacobian
+        const delta = 1e-4;
+        let J = new Array(this.dof);
+        for (let i = 0; i < this.dof; i++) {
+            J[i] = new Array(3).fill(0);
+        }
+        const left_orig_pos = this.get_left_end_effector_position();
+        // TODO: Implement your Jacobian here
+        for (let i = 0; i < this.dof; i++){
+            this.left_theta[i] += delta;
+            this.apply_theta();
+            const left_new_pos = this.get_left_end_effector_position();
+            for (let j = 0; j < 3; j++)
+            J[i][j] = (left_new_pos[j] - left_orig_pos[j]) / delta;
+        this.apply_theta();
+        this.left_theta[i] -= delta;
+        }
+        return J; // 3x7 in my case.
+    }
+
+    calculate_right_leg_Jacobian() {
+        // use newton's method in order to find the jacobian
+        const delta = 1e-4;
+        let J = new Array(this.dof);
+        for (let i = 0; i < this.dof; i++) {
+            J[i] = new Array(3).fill(0);
+        }
+        const right_orig_pos = this.get_right_leg_end_effector_position();
+        // TODO: Implement your Jacobian here
+        for (let i = 0; i < this.dof; i++){
+            this.right_leg_theta[i] += delta;
+            this.apply_theta();
+            const right_new_pos = this.get_right_leg_end_effector_position();
+            for (let j = 0; j < 3; j++)
+            J[i][j] = (right_new_pos[j] - right_orig_pos[j]) / delta;
+        this.apply_theta();
+        this.right_leg_theta[i] -= delta;
+        }
+        return J; // 3x7 in my case.
+    }
+
+    calculate_inverse_Jacobian(J){
+        // use SVD in order to forgo singularities entirely
+        var singular_value_decomposition = numeric.svd(J);
+        const a = singular_value_decomposition.U;
+        const b = singular_value_decomposition.S;
+        const c = singular_value_decomposition.V;
+        var pseudoinverse = numeric.dot(numeric.dot(c, numeric.diag(b.map(value => (value !== 0) ? 1 / value : -1))), numeric.transpose(a));
+        if (J[0].length > J.length) {
+            return numeric.transpose(pseudoinverse);
+        } else {
+            return pseudoinverse;
+        }
+    }
+
+    calculate_delta_theta(J, dx) {
+        const A = math.multiply(math.transpose(J), J);
+        // console.log(A);
+        // console.log(dx);
+        const b = math.multiply(math.transpose(J), dx);
+        // const b = math.multiply(math.transpose(J), dx);
+        // console.log(b);
+        const x = math.lusolve(A, b);
+        console.log(x);
+
+        return x;
+    }
+
+    get_right_end_effector_position() {
+        // in this example, we only have one end effector.
+        this.matrix_stack = [];
+        this._rec_update(this.root, Mat4.identity());
+        const v = this.right_end_effector.global_position; // vec4
+        return vec3(v[0], v[1], v[2]);
+    }
+
+    get_right_leg_end_effector_position() {
+        // in this example, we only have one end effector.
+        this.matrix_stack = [];
+        this._rec_update(this.root, Mat4.identity());
+        const v = this.right_leg_end_effector.global_position; // vec4
+        return vec3(v[0], v[1], v[2]);
+    }
+
+    get_left_end_effector_position() {
+        // in this example, we only have one end effector.
+        this.matrix_stack = [];
+        this._rec_update(this.root, Mat4.identity());
+        const v = this.left_end_effector.global_position; // vec4
+        return vec3(v[0], v[1], v[2]);
+    }
+
+    _rec_update(arc, matrix) {
+        if (arc !== null) {
+            const L = arc.location_matrix;
+            const A = arc.articulation_matrix;
+            matrix.post_multiply(L.times(A));
+            this.matrix_stack.push(matrix.copy());
+
+            if (arc.end_effector !== null) {
+                arc.end_effector.global_position = matrix.times(arc.end_effector.local_position);
+            }
+
+            const node = arc.child_node;
+            const T = node.transform_matrix;
+            matrix.post_multiply(T);
+
+            matrix = this.matrix_stack.pop();
+            for (const next_arc of node.children_arcs) {
+                this.matrix_stack.push(matrix.copy());
+                this._rec_update(next_arc, matrix);
+                matrix = this.matrix_stack.pop();
+            }
+        }
+    }
+
+    draw(webgl_manager, uniforms, material) {
+        this.matrix_stack = [];
+        this._rec_draw(this.root, Mat4.identity(), webgl_manager, uniforms, material);
+    }
+
+    _rec_draw(arc, matrix, webgl_manager, uniforms, material) {
+        if (arc !== null) {
+            const L = arc.location_matrix;
+            const A = arc.articulation_matrix;
+            matrix.post_multiply(L.times(A));
+            this.matrix_stack.push(matrix.copy());
+
+            const node = arc.child_node;
+            const T = node.transform_matrix;
+            matrix.post_multiply(T);
+            node.shape.draw(webgl_manager, uniforms, matrix, material);
+
+            matrix = this.matrix_stack.pop();
+            for (const next_arc of node.children_arcs) {
+                this.matrix_stack.push(matrix.copy());
+                this._rec_draw(next_arc, matrix, webgl_manager, uniforms, material);
+                matrix = this.matrix_stack.pop();
+            }
+        }
+    }
+}
+
+class Node {
+    constructor(name, shape, transform) {
+        this.name = name;
+        this.shape = shape;
+        this.transform_matrix = transform;
+        this.children_arcs = [];
+    }
+}
+
+class Arc {
+    constructor(name, parent, child, location) {
+        this.name = name;
+        this.parent_node = parent;
+        this.child_node = child;
+        this.location_matrix = location;
+        this.articulation_matrix = Mat4.identity();
+        this.end_effector = null;
+        this.dof = {
+            Rx: false,
+            Ry: false,
+            Rz: false,
+        }
+    }
+
+    // Here I only implement rotational DOF
+    set_dof(x, y, z) {
+        this.dof.Rx = x;
+        this.dof.Ry = y;
+        this.dof.Rz = z;
+    }
+
+    update_articulation(theta) {
+        this.articulation_matrix = Mat4.identity();
+        let index = 0;
+        if (this.dof.Rx) {
+            this.articulation_matrix.pre_multiply(Mat4.rotation(theta[index], 1, 0, 0));
+            index += 1;
+        }
+        if (this.dof.Ry) {
+            this.articulation_matrix.pre_multiply(Mat4.rotation(theta[index], 0, 1, 0));
+            index += 1;
+        }
+        if (this.dof.Rz) {
+            this.articulation_matrix.pre_multiply(Mat4.rotation(theta[index], 0, 0, 1));
+        }
+    }
+}
+
+class End_Effector {
+    constructor(name, parent, local_position) {
+        this.name = name;
+        this.parent = parent;
+        this.local_position = local_position;
+        this.global_position = null;
+    }
+}
