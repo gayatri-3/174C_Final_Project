14ccdbaa1484dcf5764fd95d12f64d5b64ecd821
AR2302 <87814445+AR2302@users.noreply.github.com>
2024-03-16 18:18:31 -0700

    Added orientation for car and coaster look fixed - still havin minor issues gotta fix

--- Rollercoaster.js
+++ Rollercoaster.js
@@ -73,50 +73,6 @@ export class Spline {
         return P_0.times(coeff_1).plus(v_0.times(coeff_2*scaler).plus(P_1.times(coeff_3).plus(v_1.times(coeff_4*scaler))));
     }
 
-    get_tangent(t){
-        if (this.sample_size < 2){
-            return vec3(0, 0, 0)
-        }
-
-        const a = Math.floor(t*(this.sample_size - 1));
-        const b = Math.ceil(t*(this.sample_size - 1));
-        const s = (t * (this.sample_size - 1)) % 1.0;
-
-        let P_0 = this.points[a].copy();
-        let v_0 = this.tangents[a].copy();
-        let P_1 = this.points[b].copy();
-        let v_1 = this.tangents[b].copy();
-
-        let coeff_1 = (6 * s*s) - (6 * s);
-        let coeff_2 = (3*s*s) - (4 * s) + 1;
-        let coeff_3 = (-6 * s*s) + (6 * s);
-        let coeff_4 = (3*s*s) - (2*s);
-        let scaler = 1 / (this.sample_size - 1);
-        return P_0.times(coeff_1).plus(v_0.times(coeff_2*scaler).plus(P_1.times(coeff_3).plus(v_1.times(coeff_4*scaler))));
-    }
-
-    get_curvature(t){
-        if (this.sample_size < 2){
-            return vec3(0, 0, 0)
-        }
-
-        const a = Math.floor(t*(this.sample_size - 1));
-        const b = Math.ceil(t*(this.sample_size - 1));
-        const s = (t * (this.sample_size - 1)) % 1.0;
-
-        let P_0 = this.points[a].copy();
-        let v_0 = this.tangents[a].copy();
-        let P_1 = this.points[b].copy();
-        let v_1 = this.tangents[b].copy();
-
-        let coeff_1 = (12*s) - 6;
-        let coeff_2 = (6*s) - 4;
-        let coeff_3 = (-12 * s) + 6;
-        let coeff_4 = (6*s) - 2;
-        let scaler = 1 / (this.sample_size - 1);
-        return P_0.times(coeff_1).plus(v_0.times(coeff_2*scaler).plus(P_1.times(coeff_3).plus(v_1.times(coeff_4*scaler))));
-    }
-
     get_points(){
         return this.points;
     }
@@ -188,18 +144,29 @@ export class Rollercoaster{
         this.spline.add_point(-5.5, 5.5, -25.0, -20.0, -20.0, 0.0);
         this.spline.add_point(-6.0, 4.5, -25.0, -20.0, -20.0, 0.0);
         this.spline.add_point(-6.5, 3.5, -25.0, -20, -20.0, 0.0);
-        this.spline.add_point(-7.0, 2.5, -25.0, -20, -20.0, 0.0);
-        this.spline.add_point(-8.0, 2.0, -25.0, -100.0, -100.0, -500.0);
+        this.spline.add_point(-7.0, 2.5, -25.0, -100.0, -100.0, 0.0);
+        this.spline.add_point(-9.0, 2.0, -25.0, -300.0, 0.0, 0.0);
+        this.spline.add_point(-14.0, 2.0, -30.0, 0.0, 0.0, -300.0);
+        this.spline.add_point(-9.0, 2.0, -35.0, 100.0, 0.0, 0.0);
         this.spline.add_point(-7.0, 2.0, -35.0, 500, 0.0, 0.0);
+        //loop
         this.spline.add_point(0.0, 2.0, -35.0, 500.0, 0.0, 0.0);
         this.spline.add_point(7.0, 10.0, -37.0, 0.0, 700.0, 0.0);
         this.spline.add_point(0.0, 20.0, -39.0, -700.0, 0.0, 0.0);
         this.spline.add_point(-7.0, 10.0, -41.0, 0.0, -700.0, 0.0);
         this.spline.add_point(0.0, 2.0, -41.0, 700.0, 0.0, 0.0);
+        //right side slow down
+        // this.spline.add_point(5.0, 2.0, -41.0, 30.0, 0.0, 0.0);
+        // this.spline.add_point(10.0, 2.0, -41.0, 30.0, 0.0, 0.0);
+        // this.spline.add_point(15.0, 2.0, -41.0, 100.0, 0.0, 200.0);
+        // this.spline.add_point(15.0, 2.0, -25.0, -500.0, 200.0, 0.0);
+
         this.spline.add_point(5.0, 2.0, -41.0, 30.0, 0.0, 0.0);
-        this.spline.add_point(10.0, 2.0, -41.0, 30.0, 0.0, 0.0);
-        this.spline.add_point(15.0, 2.0, -41.0, 100.0, 0.0, 200.0);
+        this.spline.add_point(20.0, 2.0, -41.0, 200.0, 0.0, 0.0);
+        this.spline.add_point(24.0, 2.0, -33.0, 0.0, 0.0, 500.0);
+        this.spline.add_point(20.0, 2.0, -25.0, -100.0, 0.0, 0.0);
         this.spline.add_point(15.0, 2.0, -25.0, -500.0, 200.0, 0.0);
+
 
         // Left Track
         this.spline_left.add_point(15.0, 2.0, -24.5, 0.0, 0.0, 0.0);
@@ -245,17 +212,26 @@ export class Rollercoaster{
         this.spline_left.add_point(-5.5, 5.5, -24.5, -20.0, -20.0, 0.0);
         this.spline_left.add_point(-6.0, 4.5, -24.5, -20.0, -20.0, 0.0);
         this.spline_left.add_point(-6.5, 3.5, -24.5, -20, -20.0, 0.0);
-        this.spline_left.add_point(-7.0, 2.5, -24.5, -20, -20.0, 0.0);
-        this.spline_left.add_point(-8.0, 2.0, -24.5, -100.0, -100.0, -500.0);
+        this.spline_left.add_point(-7.0, 2.5, -24.5, -100.0, -100.0, 0.0);
+        this.spline_left.add_point(-9.0, 2.0, -24.5, -300.0, 0.0, 0.0);
+        this.spline_left.add_point(-14.0, 2.0, -29.5, 0.0, 0.0, -300.0);
+        this.spline_left.add_point(-9.0, 2.0, -34.5, 100.0, 0.0, 0.0);
         this.spline_left.add_point(-7.0, 2.0, -34.5, 500, 0.0, 0.0);
+        //loop
         this.spline_left.add_point(0.0, 2.0, -34.5, 500.0, 0.0, 0.0);
         this.spline_left.add_point(7.0, 10.0, -36.5, 0.0, 700.0, 0.0);
         this.spline_left.add_point(0.0, 20.0, -38.5, -700.0, 0.0, 0.0);
         this.spline_left.add_point(-7.0, 10.0, -40.5, 0.0, -700.0, 0.0);
         this.spline_left.add_point(0.0, 2.0, -40.5, 700.0, 0.0, 0.0);
+        // this.spline_left.add_point(5.0, 2.0, -40.5, 30.0, 0.0, 0.0);
+        // this.spline_left.add_point(10.0, 2.0, -40.5, 30.0, 0.0, 0.0);
+        // this.spline_left.add_point(15.0, 2.0, -40.5, 100.0, 0.0, 200.0);
+        // this.spline_left.add_point(15.0, 2.0, -24.5, -500.0, 200.0, 0.0);
+
         this.spline_left.add_point(5.0, 2.0, -40.5, 30.0, 0.0, 0.0);
-        this.spline_left.add_point(10.0, 2.0, -40.5, 30.0, 0.0, 0.0);
-        this.spline_left.add_point(15.0, 2.0, -40.5, 100.0, 0.0, 200.0);
+        this.spline_left.add_point(20.0, 2.0, -40.5, 200.0, 0.0, 0.0);
+        this.spline_left.add_point(24.0, 2.0, -32.5, 0.0, 0.0, 500.0);
+        this.spline_left.add_point(20.0, 2.0, -24.5, -100.0, 0.0, 0.0);
         this.spline_left.add_point(15.0, 2.0, -24.5, -500.0, 200.0, 0.0);
 
         // Right Track
@@ -302,17 +278,26 @@ export class Rollercoaster{
         this.spline_right.add_point(-5.5, 5.5, -25.5, -20.0, -20.0, 0.0);
         this.spline_right.add_point(-6.0, 4.5, -25.5, -20.0, -20.0, 0.0);
         this.spline_right.add_point(-6.5, 3.5, -25.5, -20, -20.0, 0.0);
-        this.spline_right.add_point(-7.0, 2.5, -25.5, -20, -20.0, 0.0);
-        this.spline_right.add_point(-8.0, 2.0, -25.5, -100.0, -100.0, -500.0);
+        this.spline_right.add_point(-7.0, 2.5, -25.5, -100.0, -100.0, 0.0);
+        this.spline_right.add_point(-9.0, 2.0, -25.5, -300.0, 0.0, 0.0);
+        this.spline_right.add_point(-14.0, 2.0, -30.5, 0.0, 0.0, -300.0);
+        this.spline_right.add_point(-9.0, 2.0, -35.5, 100.0, 0.0, 0.0);
         this.spline_right.add_point(-7.0, 2.0, -35.5, 500, 0.0, 0.0);
+        //loop
         this.spline_right.add_point(0.0, 2.0, -35.5, 500.0, 0.0, 0.0);
         this.spline_right.add_point(7.0, 10.0, -37.5, 0.0, 700.0, 0.0);
         this.spline_right.add_point(0.0, 20.0, -39.5, -700.0, 0.0, 0.0);
         this.spline_right.add_point(-7.0, 10.0, -41.5, 0.0, -700.0, 0.0);
         this.spline_right.add_point(0.0, 2.0, -41.5, 700.0, 0.0, 0.0);
+        // this.spline_right.add_point(5.0, 2.0, -41.5, 30.0, 0.0, 0.0);
+        // this.spline_right.add_point(10.0, 2.0, -41.5, 30.0, 0.0, 0.0);
+        // this.spline_right.add_point(15.0, 2.0, -41.5, 100.0, 0.0, 200.0);
+        // this.spline_right.add_point(15.0, 2.0, -25.5, -500.0, 200.0, 0.0);
+
         this.spline_right.add_point(5.0, 2.0, -41.5, 30.0, 0.0, 0.0);
-        this.spline_right.add_point(10.0, 2.0, -41.5, 30.0, 0.0, 0.0);
-        this.spline_right.add_point(15.0, 2.0, -41.5, 100.0, 0.0, 200.0);
+        this.spline_right.add_point(20.0, 2.0, -41.5, 200.0, 0.0, 0.0);
+        this.spline_right.add_point(24.0, 2.0, -33.5, 0.0, 0.0, 500.0);
+        this.spline_right.add_point(20.0, 2.0, -25.5, -100.0, 0.0, 0.0);
         this.spline_right.add_point(15.0, 2.0, -25.5, -500.0, 200.0, 0.0);
         //Initializing tracks
         const curve = (t) => this.spline.get_position(t);
@@ -355,11 +340,13 @@ export class Rollercoaster{
         let points_left = [vec3(15.0, 2.0, -24.5), vec3(14.0, 3.0, -24.5), vec3(13.0, 4.0, -24.5), vec3(12.0, 5.0, -24.5),
             vec3(11.0, 6.0, -24.5), vec3(10.0, 7.0, -24.5), vec3(9.0, 8.0, -24.5), vec3(8.0, 9.0, -24.5), vec3(7.0, 10.0, -24.5),
             vec3(6.0, 11.0, -24.5), vec3(5.0, 12.0, -24.5), vec3(4.0, 13.0, -24.5), vec3(3.0, 14.0, -24.5), vec3(2.0, 15.0, -24.5),
-            vec3(1.0, 16.0, -24.5), vec3(0.0, 16.6, -25.5), vec3(-0.5,15.5, -24.5), vec3(-1.5, 13.5, -24.5), vec3(-2.5, 11.5, -24.5), vec3(-3.5, 9.5, -24.5),
-            vec3(-4.5, 7.5, -24.5), vec3(-5.5, 5.5, -24.5), vec3(-6.5, 3.5, -24.5), vec3(-7.0, 2.5, -24.5), vec3(-8.0, 2.0, -24.5), vec3(-7.0, 2.0, -34.5),
-            vec3(0.0, 2.0, -34.5), vec3(0.0, 2.0, -40.5), vec3(5.0, 2.0, -40.5), vec3(10.0, 2.0, -40.5), vec3(15.0, 2.0, -40.5)];
+            vec3(1.0, 16.0, -24.5), vec3(0.0, 16.6, -25.5), vec3(-0.5,15.5, -24.5), vec3(-1.5, 13.5, -24.5), vec3(-2.5, 11.5, -24.5),
+            vec3(-3.5, 9.5, -24.5), vec3(-4.5, 7.5, -24.5), vec3(-5.5, 5.5, -24.5), vec3(-6.5, 3.5, -24.5), vec3(-7.0, 2.5, -24.5),
+            vec3(-9.0, 2.0, -24.5), vec3(-14.0, 2.0, -29.5), vec3(-9.0, 2.0, -34.5),
+            vec3(-7.0, 2.0, -34.5), vec3(0.0, 2.0, -34.5), vec3(0.0, 2.0, -40.5), vec3(5.0, 2.0, -40.5), vec3(20.0, 2.0, -40.5),
+            vec3(24.0, 2.0, -32.5), vec3(20.0, 2.0, -24.5), vec3(15.0, 2.0, -40.5)];
 
-        for (let i = 0; i < 30; i++){
+        for (let i = 0; i < points_left.length; i++){
             const p1 = points_left[i];
             const p2 = vec3(points_left[i][0], 0.0, points_left[i][2]);
             const len = (p2.minus(p1)).norm();
@@ -404,11 +391,13 @@ export class Rollercoaster{
         points_left = [vec3(15.0, 2.0, -25.5), vec3(14.0, 3.0, -25.5), vec3(13.0, 4.0, -25.5), vec3(12.0, 5.0, -25.5),
             vec3(11.0, 6.0, -25.5), vec3(10.0, 7.0, -25.5), vec3(9.0, 8.0, -25.5), vec3(8.0, 9.0, -25.5), vec3(7.0, 10.0, -25.5),
             vec3(6.0, 11.0, -25.5), vec3(5.0, 12.0, -25.5), vec3(4.0, 13.0, -25.5), vec3(3.0, 14.0, -25.5), vec3(2.0, 15.0, -25.5),
-            vec3(1.0, 16.0, -25.5), vec3(0.0, 16.6, -25.5), vec3(-0.5,15.5, -25.5), vec3(-1.5, 13.5, -25.5), vec3(-2.5, 11.5, -25.5), vec3(-3.5, 9.5, -25.5),
-            vec3(-4.5, 7.5, -25.5), vec3(-5.5, 5.5, -25.5), vec3(-6.5, 3.5, -25.5), vec3(-7.0, 2.5, -25.5), vec3(-8.0, 2.0, -25.5), vec3(-7.0, 2.0, -35.5),
-            vec3(0.0, 2.0, -35.5), vec3(0.0, 2.0, -41.5), vec3(5.0, 2.0, -41.5), vec3(10.0, 2.0, -41.5), vec3(15.0, 2.0, -41.5)];
+            vec3(1.0, 16.0, -25.5), vec3(0.0, 16.6, -25.5), vec3(-0.5,15.5, -25.5), vec3(-1.5, 13.5, -25.5), vec3(-2.5, 11.5, -25.5),
+            vec3(-3.5, 9.5, -25.5), vec3(-4.5, 7.5, -25.5), vec3(-5.5, 5.5, -25.5), vec3(-6.5, 3.5, -25.5), vec3(-7.0, 2.5, -25.5),
+            vec3(-9.0, 2.0, -25.5), vec3(-14.0, 2.0, -30.5), vec3(-9.0, 2.0, -35.5),
+            vec3(-7.0, 2.0, -35.5), vec3(0.0, 2.0, -35.5), vec3(0.0, 2.0, -41.5), vec3(5.0, 2.0, -41.5), vec3(20.0, 2.0, -41.5),
+            vec3(24.0, 2.0, -33.5), vec3(20.0, 2.0, -25.5)];
 
-        for (let i = 0; i < 30; i++){
+        for (let i = 0; i < points_left.length; i++){
             const p1 = points_left[i];
             const p2 = vec3(points_left[i][0], 0.0, points_left[i][2]);
             const len = (p2.minus(p1)).norm();
@@ -452,7 +441,7 @@ export class Rollercoaster{
 
     draw_coaster(caller, uniforms, materials){
         if(this.stage === 0){
-            this.main_ride_sample_size = 700;
+            this.main_ride_sample_size = 600;
         }
         if(this.stage === 1){
             this.main_ride_sample_size -= 1;
@@ -464,12 +453,12 @@ export class Rollercoaster{
         let spline_position = this.spline.get_position(this.spline_t);
 
         if(spline_position[1] > 16.0 && spline_position[0] <= 0.0 && this.stage === 0){
-            this.main_ride_sample_size = 200;
+            this.main_ride_sample_size = 100;
             this.stage = 1;
         }
 
         if(spline_position[0] < -6.8 && spline_position[1] < 2.7 && this.stage === 1){
-            this.main_ride_sample_size = 500;
+            this.main_ride_sample_size = 250;
             this.stage = 2;
         }
 
@@ -479,10 +468,9 @@ export class Rollercoaster{
         }
 
         if(spline_position[0] === 15.0 && spline_position[1] === 2.0 && spline_position[2] > -26.0 && this.stage === 3){
-            this.main_ride_sample_size = 700;
+            this.main_ride_sample_size = 600;
             this.stage = 0;
         }
-        // let point1 = this.spline.get_position(Math.pow(Math.sin(this.t_sim / 5),2));
         // let model_transform = Mat4.identity().pre_multiply(Mat4.translation(spline_position[0], spline_position[1], spline_position[2]));
         // model_transform = model_transform.times(Mat4.scale(.5, .5, .5));
         // model_transform = model_transform.times(Mat4.translation(0, 1, 0));
